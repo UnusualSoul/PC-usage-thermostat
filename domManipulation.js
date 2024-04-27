406b47585fcdec0642feefb8e4512f3feb128d49
UnusualSoul <elsonmatthew@yahoo.ca>
2024-04-27 09:06:30 -0400

    added event listener 'DOMContentLoaded'

--- domManipulation.js
+++ domManipulation.js
@@ -1,16 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
     //const test = document.querySelectorAll("img");
     // const test2 = document.querySelectorAll("background-image");
     // for (let index = 0; index < test2.length; index++) {
     //     const element = test2[index];
     //     element.remove();   
     // }
-    
+
     const test = document.querySelector("li");
     const h1 = document.createElement("h1");
     h1.appendChild(test);
     h1.innerText = "LETS GOOOOOOOOOOOO!!!";
     console.log('h1.innerText :', h1.innerText);
-
+});
     function addMinutes(seconds) {
         if(seconds)
             return Date.now() + seconds*1000;
