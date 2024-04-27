291d5f004289e712420d7caa56769dd09290cac0
UnusualSoul <elsonmatthew@yahoo.ca>
2024-04-27 09:34:27 -0400

    remove content loaded, doesn't seem to work

--- domManipulation.js
+++ domManipulation.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
     //const test = document.querySelectorAll("img");
     // const test2 = document.querySelectorAll("background-image");
     // for (let index = 0; index < test2.length; index++) {
@@ -11,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     h1.appendChild(test);
     h1.innerText = "LETS GOOOOOOOOOOOO!!!";
     console.log('h1.innerText :', h1.innerText);
-});
+
     function addMinutes(seconds) {
         if(seconds)
             return Date.now() + seconds*1000;
