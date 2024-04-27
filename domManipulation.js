6111a97e95293a5992dfc281a21e8eb6fe9df9a3
UnusualSoul <elsonmatthew@yahoo.ca>
2024-04-27 08:48:00 -0400

    testing adding elements to DOM

--- domManipulation.js
+++ domManipulation.js
@@ -1,5 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const test = document.querySelector("body").appendChild("h1");
-    test.innerText = "LET'S GOOOOOOOOOOOO!!!";
-    console.log('test.innerText :', test.innerText);
-});
+    const test = document.querySelectorAll("img");
+    // const test2 = document.querySelectorAll("background-image");
+    // for (let index = 0; index < test2.length; index++) {
+    //     const element = test2[index];
+    //     element.remove();   
+    // }
+    
+    const h1 = test.appendChild("h1");
+    h1.innerText = "LET'S GOOOOOOOOOOOO!!!";
+    console.log('h1.innerText :', h1.innerText);
+
+    function addMinutes(seconds) {
+        if(seconds)
+            return Date.now() + seconds*1000;
+        else
+            return Date.now() + 1000 // one minute;
+    }
+    
+    //time
+    function calculateTimeRemaining(endTime){
+        return endTime - Date.now();
+    }
