ca29f117ac88326d84d60feba05a4fc89ffdf608
UnusualSoul <elsonmatthew@yahoo.ca>
2024-04-26 22:28:46 -0400

    add permissions in manifest.json

--- domManipulation.js
+++ domManipulation.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const test = document.querySelector("body").appendChild("h1");
     test.innerText = "LET'S GOOOOOOOOOOOO!!!";
+    console.log('test.innerText :', test.innerText);
 });
