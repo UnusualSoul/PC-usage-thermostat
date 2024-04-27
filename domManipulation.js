106db8364302926b0cad56828278418253f5c5bc
UnusualSoul <elsonmatthew@yahoo.ca>
2024-04-27 12:53:11 -0400

    testing

--- domManipulation.js
+++ domManipulation.js
@@ -67,8 +67,7 @@
             // alert('50% time passed! Go outside!');
             const newImage = document.createElement("img");
             newImage.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv6zt50rc9YX61OUe9DfmPSh8hgxkDWl6NxqAaaIaZm2e_4hKlL72RIvEIqeskPAkl860&usqp=CAU"; // Replace with your actual image URL
-            document.body.children[0].appendChild(newImage);
-
+            document.body.appendChild(newImage);
             hit50 = true;
         }
 
