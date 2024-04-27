f087777eccebcc9bf07ddec96ae25271bfdd9518
UnusualSoul <elsonmatthew@yahoo.ca>
2024-04-26 22:11:31 -0400

    fixed the calculation of the timer differnce and add working on DOM manipulation for the extension

---
+++ domManipulation.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const test = document.querySelector("body").appendChild("h1");
+    test.innerText = "LET'S GOOOOOOOOOOOO!!!";
+});
--- main.js
+++ main.js
@@ -1,37 +1,57 @@
 // This is js.file for pop-up html when a user clicks an extension button on browser 
 document.addEventListener('DOMContentLoaded', function() {
-  let whiteSpace = document.getElementById('white-space');
-  const redBar = document.getElementById('red-bar');
-  let submitButton = document.getElementById('countButton');
-  let resetButton = document.getElementById('reset');
+  const submitButton = document.getElementById('countButton');
+  const resetButton = document.getElementById('reset');
 
   submitButton.addEventListener('click', function() {
     let userInput = document.getElementById('userInput').value;
     let seconds = userInput * 60;
     startCountdown(seconds);
-  })
+    //const startTime = Date.now() + seconds * 1000;
+    const endTime = addMinutes(seconds);
+    const interval = setInterval(() => {
+      const timeRemaining = calculateTimeRemaining(endTime)
+      console.log(timeRemaining);
+      if(timeRemaining <= 0)
+        clearInterval(interval);
+    }, 500)
+  });
 
   resetButton.addEventListener('click', function() {
     resetCountDown();
-  })  
+  })
 
-  // start countdown 
-  function startCountdown(seconds) {
-    // this is javascript manipulating the css style transition 
-    // which is a shorthand for transition-property, transition-duration, and transition-timing-function)
-    // which in the line below are separated by spaces 
-    redBar.style.transition = "height " + seconds + "s linear";
-    redBar.style.height = whiteSpace.offsetHeight + "px";
-  }
-
-  // reset countdown 
-  function resetCountDown() {
-    redBar.style.transition = 'none';
-    redBar.style.height = '1%';
-  }
 });
 
+// start countdown 
+function startCountdown(seconds) {
+  const redBar = document.getElementById('red-bar');
+  const whiteSpace = document.getElementById('white-space');
+  // this is javascript manipulating the css style transition 
+  // which is a shorthand for transition-property, transition-duration, and transition-timing-function)
+  // which in the line below are separated by spaces 
+  redBar.style.transition = "height " + seconds + "s linear";
+  redBar.style.height = whiteSpace.offsetHeight + "px";
+}
 
+// reset countdown 
+function resetCountDown() {
+  const redBar = document.getElementById('red-bar');
+  redBar.style.transition = 'none';
+  redBar.style.height = '1%';
+}
+
+function addMinutes(seconds) {
+  if(seconds)
+    return Date.now() + seconds*1000;
+  else
+    return Date.now() + 1000 // one minute;
+}
+
+//time
+function calculateTimeRemaining(endTime){
+  return endTime - Date.now();
+}
 
 /*
 previous attempt to alert 
@@ -39,59 +59,4 @@ previous attempt to alert
     // const minutes = prompt("how many minutes do you want to spend on the computer", "50");
     // const seconds = minutes * 60;
 
-*/
-
-
-//time
-const startTime = Date.now();
-console.log('startTime :', startTime);
-const endTime = Date.now() + minutes;
-console.log('endTime :', endTime);
-
-/*
-//attempt # 2
-   const redBar = document.getElementById('red-bar');
-   redBar.style.animationName = "increaseHeight";
-   redBar.style.animationDuration = (milliseconds / 1000) + "s";
-   redBar.addEventListener('animationend', () => {
-      redBar.style.height = "100%";
-   })
-
-   //put this inside the CSS
-@keyframes increaseHeight {
-  from {
-    height: 0;
-  }
-  to {
-    height: 334px;
-  }
-}
-
-
-//attempt #1
-let whiteSpace = document.getElementById('white-space');
-let currentHeight = 0;
-const minutes = prompt("how many minutes do you want to spend on the computer", "50");
-const milliseconds = (minutes * 60 * 1000);
-//60000 ms = one minute
-const loopTime = 200;
-//increment every 2 milli seconds
-const loops = milliseconds / loopTime;
-//need 30000 loops
-
-//whiteSpace.offsetHeight
-//element height = 360
-const pxToIncrement = whiteSpace.offsetHeight / loops;
-console.time("timer")
-//height / 3000 = px need to increment each loop
-let increasingRedBar = setInterval(() => {
-  // console.log('counting')
-  currentHeight += pxToIncrement;
-  const redBar = document.getElementById('red-bar');
-  redBar.style.height = `${currentHeight}px`;
-  if (currentHeight + pxToIncrement >= whiteSpace.offsetHeight) {
-    // console.log('stop')
-    clearInterval(increasingRedBar);
-  }
-}, loopTime);
 */
